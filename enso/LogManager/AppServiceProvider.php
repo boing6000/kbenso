cff8b5bc8530a7e7c958b85e85fbb63c71e0d276
Andre Teodoro <boing6000@hotmail.com>
2018-08-08 17:05:47 -0300

    .

---
+++ enso/LogManager/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace LaravelEnso\LogManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadDependencies();
+        $this->publishesAll();
+    }
+
+    private function loadDependencies()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+    }
+
+    private function publishesAll()
+    {
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+        ], 'logs-assets');
+
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+        ], 'enso-assets');
+    }
+
+    public function register()
+    {
+        //
+    }
+}
---
+++ enso/LogManager/app/Classes/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LaravelEnso\LogManager\app\Classes;
+
+use Carbon\Carbon;
+
+abstract class Handler
+{
+    protected const LogSizeLimit = 0.5;
+
+    protected function log($file)
+    {
+        $size = $this->formattedSize(\File::size($file));
+        $file_windows = str_replace('\\', '/', $file);
+        return [
+            'path' => $file,
+            'name' => collect(explode('/', $file_windows))->last(),
+            'size' => $size,
+            'canBeSeen' => $size <= self::LogSizeLimit,
+            'modified' => Carbon::createFromTimestamp(
+                \File::lastModified($file)
+            ),
+        ];
+    }
+
+    protected function formattedSize($size)
+    {
+        return round($size / 1048576, 3);
+    }
+}
