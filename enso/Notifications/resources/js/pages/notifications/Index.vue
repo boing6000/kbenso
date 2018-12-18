6af8e7355419c23205547d309a695cf53f075c8e
Andre <boing6000@hotmail.com>
2018-12-18 16:23:03 -0200

    notifications

--- enso/Notifications/resources/js/pages/notifications/Index.vue
+++ enso/Notifications/resources/js/pages/notifications/Index.vue
@@ -1,48 +1,46 @@
 <template>
 
     <div>
-        <div class="has-text-centered"
-            v-if="!initialised && loading">
-            <h4 class="title is-4 has-text-centered">
-                {{ __('Loading') }}
-                <span class="icon is-small has-margin-left-medium">
-                    <fa icon="spinner"
-                        size="xs"
-                        spin/>
-                </span>
-            </h4>
-        </div>
-        <div class="columns is-centered"
-            v-if="notifications.length">
+        <div class="columns is-centered">
             <div class="column is-half-desktop">
                 <div class="level is-mobile">
-                    <div class="level-left">
-                        <div class="level-item">
-                            <a class="button is-small is-success"
-                                @click="markAllAsRead">
-                                <span>{{ __("Mark all as read") }}</span>
-                                <span class="icon is-small">
-                                    <fa icon="check"/>
-                                </span>
-                            </a>
-                        </div>
+                    <div class="level-item"
+                        v-if="notifications.length">
+                        <a class="button is-success is-outlined"
+                            @click="updateAll">
+                            <span>{{ __("Mark all read") }}</span>
+                            <span class="icon is-small">
+                                <fa icon="check"/>
+                            </span>
+                        </a>
                     </div>
-                    <div class="level-right">
-                        <div class="level-item is-marginless">
-                            <a class="button is-small is-warning has-margin-left-small"
-                                @click="clearAll">
-                                <span>{{ __("Clear all") }}</span>
-                                <span class="icon is-small">
-                                    <fa icon="trash-alt"/>
-                                </span>
-                            </a>
-                        </div>
+                    <div class="level-item">
+                        <a :class="['button animated fadeIn', {'is-loading': loading}]"
+                            @click="fetch">
+                            <span>{{ __('Reload') }}</span>
+                            <span class="icon is-small">
+                                <fa icon="sync"/>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="level-item is-marginless"
+                        v-if="notifications.length">
+                        <a class="button is-warning is-outlined has-margin-left-small"
+                            @click="destroyAll">
+                            <span>{{ __("Clear all") }}</span>
+                            <span class="icon is-small">
+                                <fa icon="trash-alt"/>
+                            </span>
+                        </a>
                     </div>
                 </div>
-                <ul>
-                    <li v-for="(notification, index) in notifications"
-                        :key="index">
-                        <div class="box is-radiusless has-padding-medium has-margin-bottom-small"
+                <transition-group tag="ul"
+                    enter-active-class="fadeIn"
+                    leave-active-class="fadeOut">
+                    <li class="animated"
+                        v-for="notification in notifications"
+                        :key="notification.id">
+                        <div class="box has-background-light has-padding-medium has-margin-bottom-small"
                             :class="{'is-bold': !notification.read_at}">
                             <fa :icon="notification.data.icon"
                                 v-if="notification.data.icon"/>
@@ -53,30 +51,24 @@
                                             && notification.data.path !== '#'
                                     }
                                 ]"
-                                @click="markAsRead(notification)">
+                                @click="update(notification)">
                                 {{ notification.data.body }}
                             </span>
                             <span class="is-pulled-right">
                                 {{ timeFromNow(notification.created_at) }}
-                                <a class="delete has-margin-left-medium"
-                                    @click="clear(notification, index)"/>
+                                <a class="delete has-margin-left-medium is-medium"
+                                    @click="destroy(notification, index)"/>
                             </span>
                             <span class="is-clearfix"/>
                         </div>
                     </li>
-                </ul>
-                <div class="has-text-centered has-margin-top-large">
-                    <button :class="['button animated fadeIn', {'is-loading': loading}]"
-                        @click="getData">
-                        {{ __('Load more') }}
-                    </button>
-                </div>
+                </transition-group>
+                <h4 class="title is-5 has-text-centered"
+                    v-if="!loading && !notifications.length">
+                    {{ __("You don't have any notifications") }}
+                </h4>
             </div>
         </div>
-        <h4 class="title is-5 has-text-centered"
-            v-else-if="initialised && !notifications.length">
-            {{ __("You don't have any notifications") }}
-        </h4>
     </div>
 
 </template>
@@ -86,17 +78,16 @@
 import debounce from 'lodash/debounce';
 import { mapState } from 'vuex';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBell, faCheck, faTrashAlt, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faCheck, faTrashAlt, faSpinner, faSync } from '@fortawesome/free-solid-svg-icons';
 import format from '../../modules/enso/plugins/date-fns/format';
 import formatDistance from '../../modules/enso/plugins/date-fns/formatDistance';
 
-library.add(faBell, faCheck, faTrashAlt, faSpinner);
+library.add(faBell, faCheck, faTrashAlt, faSpinner, faSync);
 
 export default {
 
     data() {
         return {
-            initialised: false,
             limit: 200,
             notifications: [],
             offset: 0,
@@ -110,43 +101,40 @@ export default {
     },
 
     created() {
-        this.getData = debounce(this.getData, 500);
-        this.getData();
+        this.fetch = debounce(this.fetch, 500);
+        this.fetch();
     },
 
     methods: {
-        getData() {
+        fetch() {
             if (this.loading) {
                 return;
             }
 
             this.loading = true;
 
-            axios.get(route(
-                'core.notifications.getList',
-                [this.offset, this.limit],
-            )).then(({ data }) => {
-                this.initialised = true;
+            axios.get(route('core.notifications.index'),
+                { params: { offset: this.offset, limit: this.limit }},
+            ).then(({ data }) => {
                 this.notifications = this.offset ? this.notifications.concat(data) : data;
                 this.offset = this.notifications.length;
                 this.loading = false;
             }).catch(error => this.handleError(error));
         },
-        markAsRead(notification) {
-            axios.patch(route('core.notifications.markAsRead', notification.id))
+        update(notification) {
+            axios.patch(route('core.notifications.update', notification.id))
                 .then(({ data }) => {
                     notification.read_at = data.read_at;
-                    this.$bus.$emit('notification-read', notification);
+                    this.$root.$emit('read-notification', notification);
                     if (notification.data.path && notification.data.path !== '#') {
                         this.$router.push({ path: notification.data.path });
                     }
                 }).catch(error => this.handleError(error));
         },
-        markAllAsRead() {
-            axios.patch(route('core.notifications.markAllAsRead'))
-                .then(() => {
-                    this.readAll();
-                }).catch(error => this.handleError(error));
+        updateAll() {
+            axios.post(route('core.notifications.updateAll'))
+                .then(() => this.readAll())
+                .catch(error => this.handleError(error));
         },
         readAll() {
             this.notifications.forEach((notification) => {
@@ -155,18 +143,18 @@ export default {
 
             this.unreadCount = 0;
 
-            this.$bus.$emit('read-all-notifications');
+            this.$root.$emit('read-all-notifications');
         },
-        clearAll() {
-            axios.patch(route('core.notifications.clearAll')).then(() => {
+        destroyAll() {
+            axios.post(route('core.notifications.destroyAll')).then(() => {
                 this.notifications = [];
-                this.$bus.$emit('clear-all-notifications');
+                this.$root.$emit('destroy-all-notifications');
             }).catch(error => this.handleError(error));
         },
-        clear(notification, index) {
-            axios.delete(route('core.notifications.clear', notification.id)).then(() => {
+        destroy(notification, index) {
+            axios.delete(route('core.notifications.destroy', notification.id)).then(() => {
                 this.notifications.splice(index, 1);
-                this.$bus.$emit('clear-notification', notification);
+                this.$root.$emit('destroy-notification', notification);
             }).catch(error => this.handleError(error));
         },
         timeFromNow(date) {
