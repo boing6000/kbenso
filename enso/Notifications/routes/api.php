2b073a754ca0c21fc663399169414ce1b0693a44
Andre <boing6000@hotmail.com>
2018-12-18 17:13:01 -0200

    Update api.php

--- enso/Notifications/routes/api.php
+++ enso/Notifications/routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
-Route::middleware(['web', 'auth', 'core'])
+Route::middleware(['web'])
     ->prefix('api/core')->as('core.')
     ->namespace('LaravelEnso\Notifications\app\Http\Controllers')
     ->group(function () {
         Route::resource('notifications', 'NotificationController', [
             'only' => ['index', 'update', 'destroy'],
         ]);
-
         Route::prefix('notifications')->as('notifications.')
             ->group(function () {
                 Route::get('count', 'NotificationController@count')
